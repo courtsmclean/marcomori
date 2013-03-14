051f4168810114af70e3579ab0ef415d17b96c19
Courtney Mclean <courtsmclean@gmail.com>
2013-03-14 09:30:06 +0000

    made index.php along with includes

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer id="site-footer">
+   <p>Copyright &copy 2010 <a href="#">Website Name</a></p>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<header>
+  <h1>Marco Mori</h1>
+
+  <nav id="site-nav">
+    <ul>
+      <li><a href="" alt="" title="">Hugo Boss Fragrance</a></li>
+      <li><a href="" alt="" title="">Lucozade Energy</a></li>
+      <li><a href="" alt="" title="">Lucozade Sport</a></li>
+      <li><a href="" alt="" title="">Lucozade Sport NFU</a></li>
+      <li><a href="" alt="" title="">Personal</a></li>
+      <li><a href="" alt="" title="">Still Life</a></li>
+    </ul>
+  </nav>
+</header>

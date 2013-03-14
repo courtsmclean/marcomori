14cd99452687de06ae26da6cdd6b2b84341c683b
Courtney Mclean <courtsmclean@gmail.com>
2013-03-14 16:37:29 +0000

    Revert "playing with styles"
    This reverts commit 6c1a41eb6f23cf4595e58d576ba35b1a2c7c0ae4.

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />  
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  
+  <title>Marco Mori</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width">
+
+  <link rel="stylesheet" href="css/normalize.min.css">
+  <link rel="stylesheet" href="css/columns.css">
+  <link rel="stylesheet" href="css/style.css" />
+  
+  <!-- scripts at bottom of page -->
+
+</head>
+
+
 <body class="homepage ">
   
-  <?php include('includes/header.php'); ?>
 
-
-<div id="sub-body">
 <section id="wrapper">
+
+<?php include('includes/header.php'); ?>
+
+
 
   <div id="container" class="centered clearfix">
 
@@ -91,7 +112,7 @@
   <?php include('includes/footer.php'); ?>
 </footer>
 
-</div>
+
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
@@ -101,7 +122,7 @@
   window.onload = function() {
         
     var wall = new Masonry( document.getElementById('container'), {
-      isFitWidth: true,
+      isFitWidth: 0,
       gutterWidth: 0
     });
     

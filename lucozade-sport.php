7b8ca97484fd47acebc8cf8396b8dfd585f31a77
Courtney Mclean <courtsmclean@gmail.com>
2013-05-29 16:48:22 +0100

    all updated files

---
+++ lucozade-sport.php
@@ -0,0 +1,100 @@
+<body class="homepage ">
+  
+<?php  
+    //Set values for page  
+    $page_title = "Marco Mori | Photography";  
+    $current_page = "home";  
+  
+    //Load header  
+    include_once('includes/header.php');  
+?>  
+
+
+
+<section class="wrapper">
+  
+  <div id="container" class="centered clearfix">
+
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/hugo_boss1.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/hugo_boss2.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/lucozade_sport_nfu1.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/lucozade_sport_nfu2.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/personal1.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/personal2.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/still_life1.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/still_life2.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/lucozade_sport1.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/lucozade_sport2.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/lucozade_energy1.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/lucozade_energy2.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/hugo_boss19.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/hugo_boss20.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/personal1.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/personal2.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/still_life1.jpg" class="scale-with-grid"></a>
+    </div>
+
+    <div class="box photo col3 masonry-brick">
+      <a href=""><img src="images/home/still_life2.jpg" class="scale-with-grid"></a>
+    </div>
+
+  </div>
+ </section><!-- #wrapper -->
+
+
+<?php include('includes/footer.php'); ?>
+
+    
+
+</body>
+</html>
